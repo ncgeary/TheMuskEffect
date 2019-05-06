ec83e20eb21f74ddd2bd40f8491915a51bcb27a6
Nicholas Geary <ncgeary@buffalo.edu>
2019-05-06 09:07:05 -0400

    .

---
+++ twitterdev.py
@@ -0,0 +1,6 @@
+import twitter
+
+api = twitter.Api(consumer_key=['yPkxrJOfDclgbpqOsGYV9pj28'],
+                  consumer_secret=['Hx1NdE8n6SW7Ju4tLSI4npJMbbK2YCLTAUVS2nXbuakDHEeB8T'],
+                  access_token_key=['1124133518676774914 - RBwtowFEGTFzr4un9MicCdTC2N1zrc'],
+                  access_token_secret=['Lnp05XhJ7V00e4zr7FgWW8oP7mAQskOgs95KukrIP6iOI'])
--- twitterpull.py
+++ twitterpull.py
@@ -7,15 +7,15 @@ Muskurl = "https://twitter.com/elonmusk"
 pagedig = urllib.request.urlopen(Muskurl)
 soup = BeautifulSoup(pagedig,"html.parser")
 
-for stamps in soup.findAll('a', ["data-time-ms"]):
-    # stamp = stamps.text
-    print(stamps)
+# for stamps in soup.findAll('a', ["data-time-ms"]):
+#     stamp = stamps.text
+#     print(stamps)
 
 #WORKS BUT NEED TO MATCH THE TWEETS TO THE DATE/TIME
 
-# for tweets in soup.findAll('p', {'class': 'TweetTextSize'}):
-#     tweet = tweets.text
-#     print(tweet)
+for tweets in soup.findAll('p', {'class': 'TweetTextSize'}):
+    tweet = tweets.text
+    print(tweet)
 
 
 
