073fcfc969eacba2a4db9a0402ddafc70bff2966
Nicholas Geary <ncgeary@buffalo.edu>
2020-02-25 18:48:04 -0500

    presentation

--- MuskHistoryData.py
+++ MuskHistoryData.py
@@ -29,7 +29,6 @@ limit = 10000
 
 tweets = ts.query_tweets_from_user(user=user, limit=limit)
 
-
 #%%
 class TweetAnalyzer():
     """
@@ -84,18 +83,16 @@ if __name__ == '__main__':
 mainData = df2.copy()
 mainData.head()
 
-
-# %%
-# mainData['Tweet_Date'] = [d.date() for d in df['Date']]
-# mainData['Tweet_Time'] = [d.time() for d in df['Date']]
-
-
 # %%
 # Truly determining what day the tweet will affect
 # Later than 4pm est then the tweet will affect the next day
 # Tweets during the day will affect the current day
 def checkDates(d):
-    if d.time().hour >= 16:
+    if d.date().weekday() == 4 and d.time().hour >= 16:  
+        return d + pd.Timedelta(days=3)
+    elif d.date().weekday() == 5:
+        return d + pd.Timedelta(days=2)
+    elif d.date().weekday() == 6:
         return d + pd.Timedelta(days=1)
     else:
         return d
@@ -104,46 +101,23 @@ def checkDates(d):
 mainData['Tweet_Date'] = mainData['Date'].apply(
     lambda d: checkDates(pd.to_datetime(d))).dt.date
 
+mainData.sort_values(by="Date")
 
-# mainData = mainData.groupby('Tweet_Date').agg(
-#     {'Likes': 'mean', 'Retweets': 'mean', 'Reply_Count': 'mean', 'Sentiment Score': 'mean'})
-
-
-# mainData = mainData.set_index('Tweet_Date')
-
-
-
+# %%
 mainData = mainData.groupby('Tweet_Date').agg(
     {'Likes': 'sum', 'Retweets': 'sum', 'Reply_Count': 'sum', 'Sentiment Score': 'mean'})
 
 # mainData.reset_index().head()
 
 # %%
-#normalizing data
-scaler = MinMaxScaler()
-
-mainData['SIZE_likes'] = scaler.fit_transform(
-    mainData['Likes'].values.reshape(-1, 1))
-mainData['SIZE_retweets'] = scaler.fit_transform(
-    mainData['Retweets'].values.reshape(-1, 1))
-mainData['SIZE_replies'] = scaler.fit_transform(
-    mainData['Reply_Count'].values.reshape(-1, 1))
-mainData['SIZE_Sent'] = scaler.fit_transform(
-    mainData['Sentiment Score'].values.reshape(-1, 1))
-
-
-mainData.info()
-
-
-
-
-# %%
 # Get the data of the stock Tesla Stock (TSLA)
-stockData = yf.download("TSLA", start="2019-4-1", end="2020-02-9")
+stockData = yf.download("TSLA", start="2019-4-1", end="2020-02-25")
 stockData.reset_index().head()
 
-stockData['SIZE_END_DAY_STOCK'] = scaler.fit_transform(
-    stockData['Close'].values.reshape(-1, 1))
+stockData = stockData.drop(columns = ['Open','High','Low','Adj Close', 'Volume'])
+
+stockData = stockData.rename(columns={"Close": "Stock_Price"})
+
 
 stockData.info()
 
@@ -152,130 +126,133 @@ stockData.info()
 #Joining Dataframes
 AllData = mainData.join(stockData, lsuffix='Tweet_Date',
                         rsuffix='Date')
-                        
-
-# AllData = AllData.drop(columns = ['Date','Tweet'])
-
-
-# AllData = AllData.drop(index='2017-04-21')
+                   
 
 AllData.head()
 
-#%%
-#Adding in moving average for likes, retweets, and replies
 
-AllData['MA_likes'] = AllData['SIZE_likes'].rolling(window=14).mean()
-AllData['MA_replies'] = AllData['SIZE_replies'].rolling(window=14).mean()
-AllData['MA_retweets'] = AllData['SIZE_retweets'].rolling(window=14).mean()
-AllData['MA_Sent'] = AllData['Sentiment Score'].rolling(window=14).mean()
+# %%
+#scaling data
+# Get column names first
+names = AllData.columns
+# Create the Scaler object
+scaler = preprocessing.StandardScaler()
+# Fit your data on the scaler object
+scaled_df = scaler.fit_transform(AllData)
 
-
-AllData.head()
-
+scaled_df = pd.DataFrame(scaled_df, columns=names)
 
 # %%
 #Real Numbers
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['SIZE_END_DAY_STOCK'],
+    x=scaled_df.index,
+    y=scaled_df['Stock_Price'],
     name="Stock Price",
     line_color='green',
     opacity=0.8))
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['Sentiment Score'],
+    x=scaled_df.index,
+    y=scaled_df['Sentiment Score'],
     name="Sentiment Score",
     line_color='red',
     opacity=0.4))
 
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['SIZE_replies'],
+    x=scaled_df.index,
+    y=scaled_df['Reply_Count'],
     name="Replies",
     line_color='blue',
     opacity=0.8))
 
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['SIZE_likes'],
+    x=scaled_df.index,
+    y=scaled_df['Likes'],
     name="Likes",
     line_color='light blue',
     opacity=0.8))
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['SIZE_retweets'],
+    x=scaled_df.index,
+    y=scaled_df['Retweets'],
     name="Retweets",
     line_color='grey',
     opacity=0.8))
 
 
 # Use date string to set xaxis range
-fig.update_layout(xaxis_range=['2019-4-1', '2020-1-1'],
-                  title_text="Elon's Stock Price vs Sentiment")
+fig.update_layout(xaxis_range=['2019-4-1', '2020-2-25'],title_text="Elon's Stock Price vs Sentiment")
 fig.show()
 
 # %%
-# Moving Average Graph
+#Elon's Stock Price vs Sentiment
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['SIZE_END_DAY_STOCK'],
+    x=scaled_df.index,
+    y=scaled_df['Stock_Price'],
     name="Stock Price",
     line_color='green',
     opacity=0.8))
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['MA_Sent'],
+    x=scaled_df.index,
+    y=scaled_df['Sentiment Score'],
     name="Sentiment Score",
     line_color='red',
     opacity=0.4))
 
+# Use date string to set xaxis range
+fig.update_layout(xaxis_range=['2019-4-1', '2020-2-25'],title_text="Elon's Stock Price vs Sentiment")
+fig.show()
+
+# %%
+#Likes, Replies, and Retweets
+fig = go.Figure()
+
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['MA_replies'],
+    x=scaled_df.index,
+    y=scaled_df['Stock_Price'],
+    name="Stock Price",
+    line_color='green',
+    opacity=0.8))
+
+fig.add_trace(go.Scatter(
+    x=scaled_df.index,
+    y=scaled_df['Reply_Count'],
     name="Replies",
-    line_color='light blue',
-    opacity=0.8))
-
-
-fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['MA_likes'],
-    name="Likes",
     line_color='blue',
-    opacity=0.8))
+    opacity=0.4))
+
 
 fig.add_trace(go.Scatter(
-    x=AllData.index,
-    y=AllData['MA_retweets'],
+    x=scaled_df.index,
+    y=scaled_df['Likes'],
+    name="Likes",
+    line_color='light blue',
+    opacity=0.4))
+
+fig.add_trace(go.Scatter(
+    x=scaled_df.index,
+    y=scaled_df['Retweets'],
     name="Retweets",
     line_color='grey',
-    opacity=0.8))
+    opacity=0.4))
+
 
 # Use date string to set xaxis range
-fig.update_layout(xaxis_range=['2019-4-1', '2020-1-1'],
-                  title_text="Elon's Stock Price vs Sentiment")
+fig.update_layout(xaxis_range=['2019-4-1', '2020-2-25'],title_text="Elon's Stock Price vs Likes, Replies, & Retweets")
 fig.show()
 
 
 
 # %%
 # Running the Covarience between the major factors
-doggo = AllData[['SIZE_likes', 'SIZE_retweets',
-                 'SIZE_replies', 'SIZE_END_DAY_STOCK', 'Sentiment Score']].copy()
-
-doggo = doggo.corr()
-
-doggo
-# %%
+doggo = meanData.corr()
 
 cdoggo = doggo.columns
 idoggo = doggo.index
@@ -285,7 +262,7 @@ CovHeat = go.Figure(data=go.Heatmap(
     x=cdoggo,
     y=idoggo    
     ))
-
+fig.update_layout(
+    title='Correlation Heat Map')
 
 CovHeat.show()
-# %%
